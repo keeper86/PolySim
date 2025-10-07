3e615ee74ab463d4d8e3e1c2ee4467ee25088e36
Tobias Kampmann <tobias.kampmann@udo.edu>
2025-10-07 15:25:09 +0200

    Introduce tooling to create react icons from svg

---
+++ scripts/svg-to-react-icons.cjs
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+// Helperscript to convert SVG icons to React components
+/* eslint-disable @typescript-eslint/no-require-imports */
+const fs = require('fs');
+const path = require('path');
+const { execSync } = require('child_process');
+
+const svgDir = path.resolve(__dirname, '../public/icons');
+const outDir = path.resolve(__dirname, '../src/components/icons');
+
+if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir, { recursive: true });
+}
+
+const files = fs.readdirSync(svgDir).filter((f) => f.endsWith('.svg'));
+
+files.forEach((svgFile) => {
+    const svgPath = path.join(svgDir, svgFile);
+    execSync(`npx @svgr/cli --typescript --icon --out-dir ${outDir} ${svgPath}`, { stdio: 'inherit' });
+});
+
+console.log(`Converted ${files.length} SVGs to React components in ${outDir}`);
