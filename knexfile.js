ea8f9ce50c9f2eb3c82efabf933f489a6be875c8
Tobias Kampmann <tobias.kampmann@udo.edu>
2025-10-08 00:06:09 +0200

    fix dev setup

--- knexfile.js
+++ knexfile.js
@@ -1,31 +1,28 @@
-const config = {
-    development: {
-        client: 'postgresql',
-        connection: {
-            connectionString: process.env.DATABASE_URL,
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            tableName: 'knex_migrations',
-        },
-    },
+// Load environment variables from .env with variable expansion support
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+const env = dotenv.config();
+dotenvExpand.expand(env);
 
-    production: {
-        client: 'postgresql',
-        connection: {
-            connectionString: process.env.DATABASE_URL,
-        },
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            tableName: 'knex_migrations',
-        },
+const defaultConfig = (overrideUrl) => ({
+    client: 'postgresql',
+    connection: {
+        connectionString: overrideUrl,
     },
+    pool: {
+        min: 2,
+        max: 10,
+    },
+    migrations: {
+        tableName: 'knex_migrations',
+    },
+});
+
+const config = {
+    development: defaultConfig(
+        `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:5432/${process.env.POSTGRES_DB}`,
+    ),
+    production: defaultConfig(process.env.DATABASE_URL),
 };
 
 export default config;
--- tests/e2e/auth.setup.ts
+++ tests/e2e/auth.setup.ts
@@ -1,12 +1,7 @@
 import { test as setup } from '@playwright/test';
+import pathToAuthStorage from './auth.path';
 
 setup('authenticate', async ({ page }) => {
-    const pathFromEnv = process.env.DEV_ONLY_PLAYWRIGHT_AUTH_STORAGE_PATH;
-    if (pathFromEnv) {
-        console.log('Using auth storage path from env:', pathFromEnv);
-    } else {
-        console.warn('DEV_ONLY_PLAYWRIGHT_AUTH_STORAGE_PATH is not set <' + pathFromEnv + '>. Tests will fail.');
-    }
     await page.goto('/');
     await page.click('text=Login');
     await page.waitForLoadState('networkidle');
@@ -16,5 +11,5 @@ setup('authenticate', async ({ page }) => {
     await page.click('button[type="submit"], button:has-text("Sign"), button:has-text("Login")');
     await page.waitForLoadState('networkidle');
 
-    await page.context().storageState({ path: pathFromEnv });
+    await page.context().storageState({ path: pathToAuthStorage });
 });
