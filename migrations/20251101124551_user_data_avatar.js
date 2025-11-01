12d178864478248f7068fd74dea0fd8cdf37648f
MikKusch <A9elSZ0K>
2025-11-01 14:03:36 +0100

    Add nullable 'avatar' binary column to 'user_data' table

---
+++ migrations/20251101124551_user_data_avatar.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.alterTable('user_data', function (table) {
+        table.binary('avatar').nullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('user_data');
+};
