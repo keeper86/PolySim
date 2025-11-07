d889f1d38ecc53c923809676ed0e111fdd96d18c
Enno Wellmann <eh.wellmann@gmx.de>
2025-11-07 11:55:41 +0100

    Update package lock json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import BeeMovie from "./BeeMovie";
+
 export default function Page() {
     return (
         <>
             <h1 className='text-2xl font-bold mb-4'>Home</h1>
-            There will be content. Promised.
+            {/* There will be content. Promised. */}
+            <BeeMovie/>
         </>
     );
 }
