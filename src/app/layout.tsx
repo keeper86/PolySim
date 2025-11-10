b8e29e51a8bec57f34172b1114f6c8b7581f7da6
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2025-11-10 12:28:36 +0100

    Back-To-Top Button, no hover ;(

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,24 +5,13 @@ import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import type { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
-import { Geist, Geist_Mono } from 'next/font/google';
 import type { ReactNode } from 'react';
 import { Toaster } from '../components/ui/sonner';
 import { authOptions } from './api/auth/[...nextauth]/authOptions';
 import AppProviders from './AppProviders';
 import './globals.css';
+import BackToTopButton from '@/components/ui/BackToTopButton';
 
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
-    display: 'swap',
-});
-
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
-    display: 'swap',
-});
 
 export const metadata: Metadata = {
     title: 'PolySim',
@@ -38,7 +27,7 @@ export default async function RootLayout({
 
     return (
         <html lang='en'>
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+            <body>
                 <AppProviders session={session}>
                     <SidebarProvider>
                         <AppSidebar />
@@ -53,6 +42,7 @@ export default async function RootLayout({
                             <main className='flex-1 p-0 sm:p-4 overflow-x-auto break-words'>{children}</main>
                             <Footer />
                         </SidebarInset>
+                        <BackToTopButton />
                     </SidebarProvider>
                     <Toaster />
                 </AppProviders>
