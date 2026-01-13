b3b9c4cb9b71bf2dc13bbaac460f2ec39c2c7814
Enno-Enno <enno.wellmann@tu.dortmund.de>
2026-01-14 00:27:01 +0100

    basic table in the query page

--- src/app/query/page.tsx
+++ src/app/query/page.tsx
@@ -2,8 +2,25 @@
 
 import { Page } from '@/components/client/Page';
 import { useTRPC } from '@/lib/trpc';
+import { ColumnDef } from '@tanstack/react-table';
+import { DataTable } from './data-table';
+
+export type AgentInTable = {
+    label: string;
+};
+
+const columns: ColumnDef<AgentInTable>[] = [
+    {
+        accessorKey: 'label',
+        header: 'Label',
+    },
+];
 
 export default function QueryPage() {
     const TRPC = useTRPC;
-    return <Page title='Query Page'></Page>;
+    return (
+        <Page title='Query Page'>
+            <DataTable columns={columns} data={[]}></DataTable>
+        </Page>
+    );
 }
