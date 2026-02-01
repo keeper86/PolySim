2abfeda17ec33d4ed71008dc96d980a2eec7b8d7
Tobias Kampmann <tobias.kampmann@udo.edu>
2026-02-01 21:47:46 +0100

    WORKING

--- src/server/controller/repository/graphQuery.ts
+++ src/server/controller/repository/graphQuery.ts
@@ -17,39 +17,15 @@ export const queryNameSchema = z
 
 type QueryInput = z.infer<GraphQuery['input']>;
 export async function runGraphQuery(db: Knex, query: GraphQuery, input: QueryInput): Promise<unknown[]> {
-    const _params = query.input.parse(input);
-    const _name = queryNameSchema.parse(query.name);
+    const params = query.input.parse(input);
+    const name = queryNameSchema.parse(query.name);
 
     await db.raw("SET search_path = ag_catalog, '$user', public");
 
-    console.log(_params);
-    const param = {
-        id: 'e8e5fac19389c6b5d4401398edce9b9a9b27d689cc92fb49dfc60c6834a0eeb2',
-    };
+    const sql = `SELECT * FROM prov.${name}(?::agtype)`;
+    const result = await db.raw(sql, [params]);
 
-    const existingId = 'b4a827afe1269ba19c71a5c2e736ed98ff5e062c76663c67761b82225ea95cfc';
-    const sql = `SELECT * FROM ag_catalog.cypher('prov', $$ MATCH (v{myid: '${existingId}'}) RETURN v $$) AS (v json);`;
+    const { rows = [] } = result as { rows?: Record<string, unknown>[] };
 
-    const result = await db.raw(
-        `
-        SELECT * FROM ag_catalog.cypher(
-            'prov', 
-            $$
-                MATCH (u $param)
-                RETURN u
-            $$, 
-            ?::agtype
-        ) AS (user_node json) 
-    `,
-        [{ param }],
-    );
-
-    console.log('RESULT', result);
-
-    const { rows = [] } = result as {
-        rows?: { v: unknown }[];
-    };
-
-    const vertices = rows.map((row) => row.v);
-    return vertices;
+    return rows.map((r) => Object.values(r)[0]);
 }
--- tests/migrations/20260201000000_add_dummy_test_function.js
+++ tests/migrations/20260201000000_add_dummy_test_function.js
@@ -1,21 +1,22 @@
 exports.up = async function (knex) {
     await knex.raw(`
     CREATE SCHEMA IF NOT EXISTS prov;
-
-
-    CREATE OR REPLACE FUNCTION prov.dummy_test_v1(params jsonb)
-    RETURNS TABLE(result jsonb)
+    CREATE OR REPLACE FUNCTION prov.dummy_test_v1(params agtype)
+    RETURNS TABLE(result json)
     LANGUAGE plpgsql
-    AS $$
+
+    AS $func$
     BEGIN
 
-        RETURN QUERY SELECT json_build_object('ok', params->>'testVal')::jsonb;
-        
+        RETURN QUERY EXECUTE
+        'SELECT * FROM ag_catalog.cypher(''prov'', $$ MATCH (u $param) RETURN u $$, $1) AS (u json)'
+        USING params;
+
     END;
-    $$;
+    $func$;
   `);
 };
 
 exports.down = async function (knex) {
-    await knex.raw(`DROP FUNCTION IF EXISTS prov.dummy_test_v1(jsonb);`);
+    await knex.raw(`DROP FUNCTION IF EXISTS prov.dummy_test_v1(agtype);`);
 };
